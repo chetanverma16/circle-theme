6587f77beae7ee5113d24efb308cd9102a75a496
Chetan Verma <chetanverma@Chetans-MacBook-Air.local>
2021-05-14 21:53:26 +0530

    Err Fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Input from "../components/Atoms/input"
-import Button from "../components/Atoms/Button"
+import Button from "../components/Atoms/button"
 import Fade from "react-reveal/Fade"
 
 const Contact = () => {
