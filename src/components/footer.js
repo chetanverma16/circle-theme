14ef397058e038f80dbc64a58dd246ab72017e70
Kamyab Rouhifar <krouhifar@myseneca.ca>
2023-12-10 23:09:09 -0500

    fixed up footer page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,63 @@
 import { Link } from "gatsby"
 import React from "react"
+import Button from "./Atoms/button"
 
 const Footer = () => {
+  const footerContent = [
+    {
+      title: "Services",
+      details: [
+        "Web Application",
+        "Mobile Application",
+        "Cloud Services (AWS)",
+      ],
+    },
+    {
+      title: "About Us",
+      details: ["Our Story", "Mission & Vision", "Leadership Team"],
+    },
+    {
+      title: "Resources",
+      details: ["Blog", "Case Studies", "FAQs"],
+    },
+    {
+      title: "Legal",
+      details: ["Privacy Policy", "Terms of Service", "Cookie Policy"],
+    },
+  ]
+
   return (
-    <div className="max-w-7xl mx-auto p-20 mt-10 bg-lightblack rounded-xl">
-      <div className="grid grid-cols-2 xxs:grid-cols-1 lg:grid-cols-2">
-        <div>
-          <h1 className="text-white text-4xl text-bold">Circle</h1>
-          <p className="text-white text-sm mt-5 opacity-50">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries.
-          </p>
-          <h1 className="text-white text-lg mt-20 ">
-            © copyright{" "}
-            <a href="https://www.chetanverma.com/">chetanverma.com</a>
-          </h1>
+    <div className="px-20 pt-20 pb-5  mt-10 bg-lightblack rounded-xl">
+      <div className="grid grid-rows-2 grid-cols-2 xxs:grid-cols-1 lg:grid-cols-3">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-white text-4xl text-bold">Techfi</h1>
+          <div>
+            <Button
+              colorClass="bg-lightBlue-400"
+              title="Contact Us"
+              textColor="text-white"
+            ></Button>
+          </div>
         </div>
-        <div className="place-self-end self-center xxs:hidden lg:block">
-          <ul className="text-xl font-poppins font-bold">
-            <li>
-              <Link to="/">Demos</Link>
-            </li>
-            <li>
-              <Link to="/">Company</Link>
-            </li>
-            <li>
-              <Link to="/">Products</Link>
-            </li>{" "}
-            <li>
-              <Link to="/">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/">Blog</Link>
-            </li>
-          </ul>
+        <div className="grid grid-cols-4 grid-flow-row-dense auto-rows-auto col-span-2">
+          {footerContent.map((item, index) => (
+            <div key={index} className="xxs:hidden lg:block col-span-1">
+              <h4 className="text-xl text-white mb-5">{item.title}</h4>
+              <ul className="flex flex-col text-sm font-poppins gap-2">
+                {item.details.map((item, index) => (
+                  <li key={index}>
+                    <Link to="/">{item}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="self-end">
+          <h1 className="text-white text-sm">
+            © 2023 <a href="https://www.techfi.ca/">techfi.ca</a>• All Rights
+            Reserved.
+          </h1>
         </div>
       </div>
     </div>
