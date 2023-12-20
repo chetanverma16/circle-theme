fdf8f0583443b2ba66dad01a0ca20d311ca087ad
Cagdas Muldur <cagdasmuldur@gmail.com>
2023-12-20 14:44:45 -0500

    Add form submission functionality to Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import Input from "../components/Atoms/input"
 import Button from "../components/Atoms/button"
 import Fade from "react-reveal/Fade"
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  // const [company, setCompany] = useState("");
+  // const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = process.env.AWS_LAMBDA + "/sendEmail";
+    const data = {
+      senderName: name,
+      senderEmail: email,
+      message: message,
+      date: new Date().toString()
+    };
+
+    fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        // Handle the API response here
+        console.log(result);
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error(error);
+      });
+  };
+
   return (
     <Layout>
       <Fade bottom cascade>
@@ -21,22 +57,24 @@ const Contact = () => {
             <form className="mt-5">
               <div className="flex xxs:flex-col sm:flex-row">
                 <div className="sm:mr-5 xxs:mr-0">
-                  <Input placeholder="Your Name"></Input>
+                  <Input value={name} onChange={e => setName(e.target.value)} placeholder="Your Name"></Input>
                 </div>
                 <div className="sm:ml-5 xxs:ml-0 xxs:mt-2 sm:mt-0">
-                  <Input placeholder="Your Email"></Input>
+                  <Input value={email} onChange={e => setEmail(e.target.value)} placeholder="Your Email"></Input>
                 </div>
               </div>
-
+{/* 
               <div className="flex mt-5 xxs:flex-col sm:flex-row">
                 <div className="sm:mr-5 xxs:mr-0">
-                  <Input placeholder="Your Company"></Input>
+                  <Input value={company} onChange={e => setCompany(e.target.value)} placeholder="Your Company"></Input>
                 </div>
                 <div className="sm:ml-5 xxs:ml-0 xxs:mt-2 sm:mt-0">
-                  <Input placeholder="Your Phone"></Input>
+                  <Input value={phone} onChange={e => setPhone(e.target.value)} placeholder="Your Phone"></Input>
                 </div>
-              </div>
+              </div> */}
               <textarea
+                value={message}
+                onChange={e => setMessage(e.target.value)}
                 className="mt-5 w-full bg-white bg-opacity-20 rounded-xl p-5"
                 rows="5"
                 placeholder="How can we help you?"
@@ -45,6 +83,7 @@ const Contact = () => {
                 title="Send us a message"
                 colorClass="bg-gradient-to-r from-pink to-purple"
                 marginClass="mt-5"
+                onClick={handleSubmit}
               ></Button>
             </form>
           </div>
