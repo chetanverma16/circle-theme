5afbfbcc88f03d139adc9bbd770a3be9b19ddf7a
Cagdas Muldur <cagdasmuldur@gmail.com>
2024-02-05 20:35:43 -0500

    Update header styles and background image height

--- src/components/Home/header.js
+++ src/components/Home/header.js
@@ -8,20 +8,20 @@ const Header = () => {
   return (
     <>
       <div className="bg-image" />
-      <div className="max-w-7xl mx-auto px-8 mt-28 mb-32 md:mt-36 md:mb-28">
+      <div className="max-w-7xl mx-auto px-8 mt-28 mb-32 md:mt-36 md:mb-60">
         <Fade>
-          <div className="flex justify-center flex-col text-center gap-4 md:mx-28 lg:mx-48">
+          <div className="flex justify-center flex-col text-center gap-6 md:mx-28 lg:mx-44">
             <h1 className=" text-white font-poppins font-semibold text-center leading-snug text-5xl sm:text-6xl lg:text-8xl">
               Innovation within
               <span className="text-gradient-header ml-3">Reach</span>
             </h1>
-            <h2 className="text-white font-poppins opacity-80 font-medium text-base xxs:text-base xs:text-lg sm:text-lg md:text-lg lg:text-lg 2xl:text-xl">
+            <h2 className="text-white font-poppins opacity-80 font-normal tracking-wide text-base xxs:text-base xs:text-lg sm:text-lg md:text-lg lg:text-2xl 2xl:text-2xl mx-36">
               We deliver premium creative solutions to your technology needs.
             </h2>
           </div>
         </Fade>
         <Fade bottom>
-          <div className="flex justify-center items-center w-full mt-12 gap-6">
+          <div className="flex justify-center items-center w-full mt-20 gap-6">
             <a
               href="https://calendly.com/techfi/30min"
               target="_blank"
